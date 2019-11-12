631759d93f47386e21174ba2cfc94e59eafe8b9a
b.gerges <b.gerges@egyptianbanks.net>
2019-11-12 09:49:53 +0200

    Prevent concurrent access to the same car

--- src/main/java/com/freenow/repository/CarRepository.java
+++ src/main/java/com/freenow/repository/CarRepository.java
@@ -1,9 +1,17 @@
 package com.freenow.repository;
 
 import com.freenow.domain.Car;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
+import java.util.Optional;
+
 @Repository
 public interface CarRepository extends CrudRepository<Car, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Override
+    Optional<Car> findById(Long aLong);
 }
